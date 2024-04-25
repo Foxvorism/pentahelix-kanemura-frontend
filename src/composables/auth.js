ab89722c30910acb0e03f0eb7f95b88cddb51526
“Foxvore” <imjohani2005@gmail.com>
2024-04-25 16:43:35 +0700

    feat: Create login function

--- src/composables/auth.js
+++ src/composables/auth.js
@@ -1,23 +1,28 @@
 import { ref } from "vue";
-// import { useCookies } from "@vueuse/integrations/useCookies";
+import dayjs from "dayjs";
+import { useCookies } from '@vueuse/integrations/useCookies'
 
 import useAxios from "./axios";
 
 const useAuth = () => {
     const user = ref("");
     const axios = useAxios();
-    // const cookies = useCookies();
+    const cookies = useCookies();
 
     const login = async (payload) => {    
         try {
             const res = await axios.post(`/auth/login`, payload);
-            console.log(await res.data);
-            // cookies.set("session-admin", await res.data);
+            console.log(await res.data.data);
+            const token = await res.data.data.token;
+            const expired = new Date(dayjs().add(30, "d"));
+            cookies.set("session-admin", token, {
+                expires: expired,
+            });
             // window.location.href = "/home";
         } catch (error) {
             console.log(error);
         } 
-    };
+    };  
 
     return {
         user,
