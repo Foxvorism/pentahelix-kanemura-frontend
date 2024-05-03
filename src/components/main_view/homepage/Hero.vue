a5305d2a30d61d1db444e7ca53170df5a491d78a
“Foxvore” <imjohani2005@gmail.com>
2024-05-03 09:08:52 +0700

    feat: Create homepage hero page

--- src/components/main_view/homepage/Hero.vue
+++ src/components/main_view/homepage/Hero.vue
@@ -1,23 +1,38 @@
 <script setup></script>
 
 <template>
-  <img
-    src="@/assets/img/hero-1.png"
-    alt="hero 1"
-    class="absolute w-full -top-3 !overflow-hidden"
-  />
-  <img
-    src="@/assets/img/hero-2.png"
-    alt="hero 2"
-    class="absolute w-[80vw] -left-[880px] rotate-6 top-7 !overflow-hidden"
-  />
+  <div class="h-[45vh] flex justify-center items-center text-center">
+    <img
+      src="@/assets/img/hero-1.png"
+      alt="hero 1"
+      class="absolute w-full -top-10 z-[2]"
+    />
+    <img
+      src="@/assets/img/hero-2.png"
+      alt="hero 2"
+      class="absolute w-[30vw] left-0 mt-[80px]"
+    />
+    <img
+      src="@/assets/img/hero-3.png"
+      alt="hero 3"
+      class="absolute w-[35vw] right-0 -top-5 z-[1]"
+    />
 
-  <div class="h-screen flex justify-center items-center text-center">
-    <div>
-      <div id="welcome">Welcome</div>
-      <div id="japanese">いらっしゃいませ</div>
+    <div class="mt-20 z-[5] text-[var(--color-blue)]">
+      <div id="welcome" class="text-8xl mb-5">Welcome</div>
+      <div id="japanese" class="text-4xl tracking-widest font-semibold">
+        いらっしゃいませ
+      </div>
     </div>
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+#welcome {
+  font-family: var(--font-japanese2020);
+}
+
+#japanese {
+  font-family: var(--font-fredoka);
+}
+</style>
