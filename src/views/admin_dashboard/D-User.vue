af16c2fa9c1d34de3d187259cc4c64ddc75373cf
“Foxvore” <imjohani2005@gmail.com>
2024-05-03 09:09:13 +0700

    feat: Add sidebar

---
+++ src/views/admin_dashboard/D-User.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { onMounted } from "vue";
+import useAuth from "@/composables/auth.js";
+import Sidebar from "@/components/admin_dashboard/Sidebar.vue";
+
+import UserData from "../../data/user.json";
+
+const list_user = UserData;
+
+const { user_info } = useAuth();
+
+onMounted(() => {
+  user_info();
+  console.log(list_user);
+});
+</script>
+
+<template>
+  <div id="body" class="w-full min-h-screen bg-[#f0f0f0]">
+    <Sidebar />
+  </div>
+</template>
+
+<style scoped></style>
