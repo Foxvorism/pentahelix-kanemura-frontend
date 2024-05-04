48743f837b3b2cb5db3d31a6cc1567a159403b25
“Foxvore” <imjohani2005@gmail.com>
2024-05-04 18:50:49 +0700

    build: Initial commit

---
+++ src/components/main_view/homepage/Rating.vue
@@ -0,0 +1,26 @@
+<script setup></script>
+
+<template>
+  <div class="mx-20 mt-20">
+    <div
+      id="title"
+      class="text-center text-xl mb-10 text-[var(--color-blue)] font-semibold"
+    >
+      People Reviews to Kanemura Japanese Food
+    </div>
+
+    <div id="rating-widget">
+      <div
+        class="elfsight-app-4e7609c8-4b59-437e-a448-3397ea87b880"
+        data-elfsight-app-lazy
+      />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+#rating-widget {
+  border-radius: 28px;
+  box-shadow: 10px 10px var(--color-blue);
+}
+</style>
