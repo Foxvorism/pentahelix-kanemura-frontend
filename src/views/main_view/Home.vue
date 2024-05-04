dfa28f5a33111cf4aac7248eda9460b1834e47f4
“Foxvore” <imjohani2005@gmail.com>
2024-05-04 18:51:17 +0700

    feat: Implementing google review widget

--- src/views/main_view/Home.vue
+++ src/views/main_view/Home.vue
@@ -2,16 +2,20 @@
 import Navbar from "@/components/main_view/Navbar.vue";
 import Hero from "@/components/main_view/homepage/Hero.vue";
 import Kanemura from "@/components/main_view/homepage/Kanemura.vue";
+import Rating from "@/components/main_view/homepage/Rating.vue";
 </script>
 
 <template>
   <Navbar />
-  <div id="main" class="w-full min-h-screen bg-[var(--color-cream)] py-32">
+  <div id="main" class="w-full min-h-screen bg-[var(--color-cream)] py-20">
     <div id="hero">
       <Hero />
     </div>
     <div id="kanemura">
       <Kanemura />
+    </div>
+    <div id="rating">
+      <Rating />
     </div>
   </div>
 </template>
