96fe35a592740953b88b67ffd3330e9c2729a475
“Foxvore” <imjohani2005@gmail.com>
2024-05-08 09:07:31 +0700

    build: Initial commit

---
+++ src/views/main_view/Order.vue
@@ -0,0 +1,15 @@
+<script setup>
+import Navbar from "@/components/main_view/Navbar.vue";
+import Hero from "@/components/main_view/order/Hero.vue";
+import Footer from "@/components/main_view/Footer.vue";
+</script>
+
+<template>
+  <Navbar />
+  <div id="main" class="w-full min-h-screen bg-[var(--color-cream)] py-20">
+    <div id="hero">
+      <Hero />
+    </div>
+  </div>
+  <Footer />
+</template>
