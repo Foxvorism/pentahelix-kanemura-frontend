874d7da62e78c2cb8cc8bfcbd9ec90045e81c7d4
“Foxvore” <imjohani2005@gmail.com>
2024-05-09 17:57:08 +0700

    feat: Create breadcrumb

--- src/views/admin_dashboard/D-Donburi.vue
+++ src/views/admin_dashboard/D-Donburi.vue
@@ -1,9 +1,26 @@
 <script setup>
 import { onMounted } from "vue";
+import { ArrowRight } from "@element-plus/icons-vue";
 import useAuth from "@/composables/auth.js";
 import Sidebar from "@/components/admin_dashboard/Sidebar.vue";
 </script>
 
 <template>
-  <Sidebar />
+  <div id="main" class="w-full min-h-screen bg-[#f0f0f0]">
+    <div id="sidebar">
+      <Sidebar />
+    </div>
+
+    <div id="body" class="pl-[256px]">
+      <div class="px-20 py-10">
+        <el-breadcrumb :separator-icon="ArrowRight">
+          <el-breadcrumb-item>Admin Dasboard</el-breadcrumb-item>
+          <el-breadcrumb-item>Menu</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '/ad-donburi' }">
+            Donburi
+          </el-breadcrumb-item>
+        </el-breadcrumb>
+      </div>
+    </div>
+  </div>
 </template>
