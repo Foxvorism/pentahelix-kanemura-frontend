a6577d06248fc3f6cc1a73e52840accd31a7000c
“Foxvore” <imjohani2005@gmail.com>
2024-05-10 20:51:18 +0700

    delete: Delete unused file

--- src/composables/cookies.js
+++
@@ -1,9 +0,0 @@
-import { useCookies } from "@vueuse/integrations/useCookies";
-const cookies = useCookies();
-
-const useSession = () => {
-    const sesi = cookies.get("session-admin");
-    return { sesi };
-};
-
-export default useSession;
