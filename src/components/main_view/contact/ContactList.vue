ae224964167edce4b5d70bba787fc0bb51519bd0
“Foxvore” <imjohani2005@gmail.com>
2024-05-21 21:29:07 +0700

    feat: Create carousel for contacts list

---
+++ src/components/main_view/contact/ContactList.vue
@@ -0,0 +1,11 @@
+<script setup></script>
+
+<template>
+  <div class="flex justify-center items-center">
+    <div class="w-3/4">
+      <el-carousel :interval="5000" type="card" height="200px">
+        <el-carousel-item v-for="item in 6" :key="item"></el-carousel-item>
+      </el-carousel>
+    </div>
+  </div>
+</template>
