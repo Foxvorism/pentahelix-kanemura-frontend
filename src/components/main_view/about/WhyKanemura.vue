6bf6c6eddaf579fb94b024c4e302c5853602a961
“Foxvore” <imjohani2005@gmail.com>
2024-05-22 23:57:05 +0700

    style: Change css to tailwind

--- src/components/main_view/about/WhyKanemura.vue
+++ src/components/main_view/about/WhyKanemura.vue
@@ -9,9 +9,24 @@
     <div class="flex item-center justify-center">
       <div>
         <div class="grid grid-cols-3 gap-32">
-          <div id="card">Best Quality Food</div>
-          <div id="card">At The Best Price</div>
-          <div id="card">Great Service</div>
+          <div
+            id="card"
+            class="px-20 py-10 bg-[var(--color-red)] text-[var(--color-cream)] text-center uppercase font-semibold tracking-wider rounded-md"
+          >
+            Best Quality Food
+          </div>
+          <div
+            id="card"
+            class="px-20 py-10 bg-[var(--color-red)] text-[var(--color-cream)] text-center uppercase font-semibold tracking-wider rounded-md"
+          >
+            At The Best Price
+          </div>
+          <div
+            id="card"
+            class="px-20 py-10 bg-[var(--color-red)] text-[var(--color-cream)] text-center uppercase font-semibold tracking-wider rounded-md"
+          >
+            Great Service
+          </div>
         </div>
       </div>
     </div>
@@ -20,17 +35,6 @@
 
 <style scoped>
 #card {
-  width: 300px;
-  height: 300px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  color: var(--color-cream);
-  background-color: var(--color-red);
-  text-transform: uppercase;
-  font-weight: 700;
-  letter-spacing: 2px;
-  border-radius: 8px;
   box-shadow: 10px 10px var(--color-blue);
 }
 </style>
