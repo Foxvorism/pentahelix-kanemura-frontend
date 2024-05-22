fcf0f1a54ffeb1c028b5ca54572cc8d375850b35
“Foxvore” <imjohani2005@gmail.com>
2024-05-22 23:59:36 +0700

    feat: Impelement menu by category page

---
+++ src/views/main_view/MenuByCategory.vue
@@ -0,0 +1,19 @@
+<script setup>
+import Navbar from "@/components/main_view/Navbar.vue";
+import HeroByCategory from "@/components/main_view/menu/HeroByCategory.vue";
+import Menus from "@/components/main_view/menu/Menus.vue";
+import Footer from "@/components/main_view/Footer.vue";
+</script>
+
+<template>
+  <Navbar />
+  <div id="main" class="w-full min-h-screen bg-[var(--color-cream)] py-20">
+    <div id="hero-by-category">
+      <HeroByCategory />
+    </div>
+    <div id="menus">
+      <Menus />
+    </div>
+  </div>
+  <Footer />
+</template>
