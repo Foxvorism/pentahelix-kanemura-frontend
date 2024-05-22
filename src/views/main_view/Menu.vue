89dd08ea6340af2c6d58387eb2a75ce109660928
“Foxvore” <imjohani2005@gmail.com>
2024-05-22 23:59:21 +0700

    feat: Implement categories list

--- src/views/main_view/Menu.vue
+++ src/views/main_view/Menu.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Navbar from "@/components/main_view/Navbar.vue";
 import Hero from "@/components/main_view/menu/Hero.vue";
+import CategoriesList from "@/components/main_view/menu/CategoriesList.vue";
 import Footer from "@/components/main_view/Footer.vue";
 </script>
 
@@ -9,6 +10,9 @@ import Footer from "@/components/main_view/Footer.vue";
   <div id="main" class="w-full min-h-screen bg-[var(--color-cream)] py-20">
     <div id="hero">
       <Hero />
+    </div>
+    <div id="categorieslist">
+      <CategoriesList />
     </div>
   </div>
   <Footer />
