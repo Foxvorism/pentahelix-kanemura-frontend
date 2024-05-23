e32ab526c32f762bec545d5514feffef5cc93ab6
“Foxvore” <imjohani2005@gmail.com>
2024-05-23 22:34:19 +0700

    feat: Create social media widget

---
+++ src/components/main_view/contact/Instagram.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="px-40">
+    <div
+      class="elfsight-app-208873de-874d-4042-be24-42622cb38041"
+      data-elfsight-app-lazy
+    ></div>
+  </div>
+</template>
---
+++ src/components/main_view/contact/Tiktok.vue
@@ -0,0 +1,10 @@
+<script setup></script>
+
+<template>
+  <div class="px-40">
+    <div
+      class="elfsight-app-42fbc1be-251d-485b-a2e3-e504ad4acdc3"
+      data-elfsight-app-lazy
+    ></div>
+  </div>
+</template>
